68f5e64c1e9e8772790ff1581372d7abadc8c5e7
JinpingMa <hyperbola615@gmail.com>
2018-02-06 20:48:00 +0800

    add component

--- src/App.js
+++ src/App.js
@@ -51,32 +51,84 @@ class App extends Component {
     const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        <form>
-          <input
-            type="text"
-            onChange={this.onSearchChange}
+        <Search
+          value={searchTerm}
+          onChange={this.onSearchChange}
           />
-        </form>
-        {list.filter(isSearched(searchTerm)).map(item =>
-          <div key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-            <button
-              onClick={()=> this.onDismiss(item.objectID)}
-              type="button">
-              Dismiss
-            </button>
-          </span>
-          </div>
-        )}
+        <Table
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+          />
+        {/*<form>*/}
+          {/*<input*/}
+            {/*type="text"*/}
+            {/*value={searchTerm}*/}
+            {/*onChange={this.onSearchChange}*/}
+          {/*/>*/}
+        {/*</form>*/}
+        {/*{list.filter(isSearched(searchTerm)).map(item =>*/}
+          {/*<div key={item.objectID}>*/}
+          {/*<span>*/}
+            {/*<a href={item.url}>{item.title}</a>*/}
+          {/*</span>*/}
+            {/*<span>{item.author}</span>*/}
+            {/*<span>{item.num_comments}</span>*/}
+            {/*<span>{item.points}</span>*/}
+            {/*<span>*/}
+            {/*<button*/}
+              {/*onClick={()=> this.onDismiss(item.objectID)}*/}
+              {/*type="button">*/}
+              {/*Dismiss*/}
+            {/*</button>*/}
+          {/*</span>*/}
+          {/*</div>*/}
+        {/*)}*/}
       </div>
     );
   }
 }
 
+class Search extends Component {
+  render() {
+    const { value, onChange } =this.props;
+    return (
+      <form>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          />
+      </form>
+    );
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item =>
+        <div key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+          <span>
+            <button
+              onClick={() => onDismiss(item.objectID)}
+              type="button"
+              >
+              Dismiss
+            </button>
+          </span>
+        </div>
+          )}
+      </div>
+    );
+  }
+}
 export default App;
