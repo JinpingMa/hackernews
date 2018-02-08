edfcba26d178d2aef00797e97bf1b2e8514418bc
JinpingMa <hyperbola615@gmail.com>
2018-02-08 14:43:44 +0800

    add css

--- src/App.js
+++ src/App.js
@@ -36,22 +36,24 @@ class App extends Component {
     const isNotId = item => item.objectID !== id;
     const updateList = this.state.list.filter(isNotId);
     this.setState({ list: updateList });
-  }
+  };
 
   onSearchChange = (event) => {
     this.setState({ searchTerm: event.target.value });
-  }
+  };
 
   render() {
     const { searchTerm, list } = this.state;
     return (
-      <div className="App">
+      <div className="page">
+        <div className="interactions">
         <Search
           value={searchTerm}
           onChange={this.onSearchChange}
           >
         Search
         </Search>
+        </div>
         <Table
           list={list}
           pattern={searchTerm}
@@ -72,7 +74,7 @@ const Search = ({ value, onChange, children }) => {
     />
     </form>
   );
-}
+};
 
 // class Search extends Component {
 //   render() {
@@ -88,49 +90,87 @@ const Search = ({ value, onChange, children }) => {
 //     );
 //   }
 // }
-
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map(item =>
-        <div key={item.objectID}>
-          <span>
+const largeColumn = {
+  width: '40%',
+};
+const midColumn = {
+  width: '30%',
+};
+const smallColumn = {
+  width: '10%',
+};
+const Table = ({ list, pattern, onDismiss }) =>
+  <div className="table">
+    {list.filter(isSearched(pattern)).map(item =>
+      <div key={item.objectID} className="table-row">
+          <span style={largeColumn}>
             <a href={item.url}>{item.title}</a>
           </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-          <span>
-            <Button onClick={() => onDismiss(item.objectID)}>
+        <span style={midColumn}>{item.author}</span>
+        <span style={smallColumn}>{item.num_comments}</span>
+        <span style={smallColumn}>{item.points}</span>
+        <span style={smallColumn}>
+            <Button
+              onClick={() => onDismiss(item.objectID)}
+              className="button-inline"
+            >
               Dismiss
             </Button>
           </span>
-        </div>
-          )}
       </div>
-    );
-  }
-}
+    )}
+  </div>;
 
-class Button extends Component {
-  render() {
-    const {
-      onClick,
-      className = '',
-      children,
-    } = this.props;
-    return (
-      <button
-        onClick={onClick}
-        className={className}
-        type="button"
-        >
-        {children}
-      </button>
-    )
-  }
-}
+// class Table extends Component {
+//   render() {
+//     const { list, pattern, onDismiss } = this.props;
+//     return (
+//       <div>
+//         {list.filter(isSearched(pattern)).map(item =>
+//         <div key={item.objectID}>
+//           <span>
+//             <a href={item.url}>{item.title}</a>
+//           </span>
+//           <span>{item.author}</span>
+//           <span>{item.num_comments}</span>
+//           <span>{item.points}</span>
+//           <span>
+//             <Button onClick={() => onDismiss(item.objectID)}>
+//               Dismiss
+//             </Button>
+//           </span>
+//         </div>
+//           )}
+//       </div>
+//     );
+//   }
+// }
 
+// class Button extends Component {
+//   render() {
+//     const {
+//       onClick,
+//       className = '',
+//       children,
+//     } = this.props;
+//     return (
+//       <button
+//         onClick={onClick}
+//         className={className}
+//         type="button"
+//         >
+//         {children}
+//       </button>
+//     )
+//   }
+// }
+
+const Button = ({onClick, className, children}) =>
+  <button
+    onClick={onClick}
+    className={className}
+    type="button"
+  >
+    {children}
+  </button>;
 export default App;
