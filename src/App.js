e88a90e7651e380aa8800b3f3e1d004e58502bfe
JinpingMa <hyperbola615@gmail.com>
2018-02-09 11:01:08 +0800

    add api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-  ];
+const DEFAULT_QUERY= 'redux';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+// const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+
+// const list = [
+//   {
+//     title: 'React',
+//     url: 'https://facebook.github.io/react/',
+//     author: 'Jordan Walke',
+//     num_comments: 3,
+//     points: 4,
+//     objectID: 0,
+//   },
+//   {
+//     title: 'Redux',
+//     url: 'https://github.com/reactjs/redux',
+//     author: 'Dan Abramov, Andrew Clark',
+//     num_comments: 2,
+//     points: 5,
+//     objectID: 1,
+//   }
+//   ];
 
 const isSearched = searchTerm => item =>
   item.title.toLowerCase().includes(searchTerm.toLowerCase());
@@ -27,15 +33,41 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list,
-      searchTerm: '',
+      // list,
+      searchTerm: DEFAULT_QUERY,
+      result: null,
     };
+
+    // this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    // this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
+    // this.onSearchChange = this.onSearchChange.bind(this);
+    // this.onDismiss = this.onDismiss.bind(this);
+
+  }
+
+  setSearchTopStories = (result) => {
+    this.setState({ result });
+  }
+
+  fetchSearchTopStories = (searchTerm) => {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(e =>e );
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
   }
 
   onDismiss = (id) => {
     const isNotId = item => item.objectID !== id;
-    const updateList = this.state.list.filter(isNotId);
-    this.setState({ list: updateList });
+    const updateHits = this.state.result.hits.filter(isNotId);
+    this.setState({
+      // result: Object.assign({}, this.state.result, { hits: updateHits})
+      result: { ...this.state.result, hits: updateHits }
+    });
   };
 
   onSearchChange = (event) => {
@@ -43,7 +75,8 @@ class App extends Component {
   };
 
   render() {
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+    if(!result) {return null;}
     return (
       <div className="page">
         <div className="interactions">
@@ -55,7 +88,7 @@ class App extends Component {
         </Search>
         </div>
         <Table
-          list={list}
+          list={result.hits}
           pattern={searchTerm}
           onDismiss={this.onDismiss}
           />
